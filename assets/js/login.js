f4b2550afad884d63f9fc7545607ca5c67b26a2c
Lucky-cabbage <2199435336@qq.com>
2022-05-30 16:01:36 +0800

    完成了登录和注册的功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,64 +9,60 @@ $(function () {
     $(".login-box").show();
     $(".reg-box").hide();
   });
-
-  // 从 LayUI 中获取 form 对象
-  const form = layui.form;
-
-  // 通过 form.verify() 方法自定义校验规则
+  // 引入form 模块
+  const form = layui.form
+  // 自定义校验规则
   form.verify({
-    // 自定义一个叫 pwd 的校验规则
-    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
-    // 校验两次密码是否一致的规则
-    repwd: (val) => {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      const pwd = $(".reg-box [name=password").val();
-      if (pwd !== val) return "两次密码不一致";
+    // 密码校验规则
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位,且不能出现空格"],
+    // 确认密码校验规则
+    repwd: (value) => {
+      // 1.获取当前输入的值
+      //2.获取密码框的值
+      //3.两者进行判断
+      //4.如果不一致,提示消息
+      const pwd = $("#form_reg [name=password]").val();
+      if (pwd !== value) return "两次密码不一致"
     },
   });
+  // 设置baseUrl
 
-  // 获取 layui 弹窗
-  const layer = layui.layer;
-  // 设置请求根路径
-  const baseUrl = "http://www.liulongbin.top:3007";
-
-  // 监听注册表单，发送注册请求
-  $("#form_reg").on("submit", (e) => {
+  // 注册功能
+  $('#form_reg').on('submit', (e) => {
+    // 阻止默认提交事件
     e.preventDefault();
     $.ajax({
-      type: "POST",
-      url: "/api/reguser",
+      type: 'POST',
+      url: '/api/reguser',
       data: {
-        username: $("#form_reg [name=username").val(),
-        password: $("#form_reg [name=password").val(),
+        username: $('#form_reg [name=username]').val(),
+        password: $('#form_reg [name=password]').val(),
       },
       success: (res) => {
-        if (res.status !== 0) return layer.msg(res.message);
-        layer.msg("注册成功！");
-        // 注册成功后跳转到登录界面
-        $("#link_login").click();
-      },
-    });
-  });
-
-  // 监听登录表单，发送登录请求
-  $("#form_login").submit((e) => {
-    e.preventDefault();
+        if (res.status !== 0) return layer.msg('注册失败')
+        layer.msg('注册成功')
+        // 模拟点击跳转登录
+        $('#link_login').click()
+      }
+    })
+  })
+  // 登录功能
+  $('#form_login').on('submit', function (e) {
+    e.preventDefault()
     $.ajax({
-      type: "POST",
-      url: "/api/login",
-      data: $("#form_login").serialize(),
+      type: 'POST',
+      url: '/api/login',
+      data: $(this).serialize(),
       success: (res) => {
-        if (res.status !== 0) return layer.msg(res.message);
-        layer.msg("登录成功！");
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem("token", res.token);
+        if (res.status !== 0) return layer.msg('登陆失败')
+        layer.msg('登陆成功')
+        // 登录成功后需要吧token令牌存放在本地
+        // 本地存储的集项里面的令牌
+        // token 登录的唯一标识
+        localStorage.setItem('token', res.token)
         // 跳转到主页
-        location.href = "/index.html";
-      },
-    });
-  });
+        location.href = '/index.html'
+      }
+    })
+  })
 });
