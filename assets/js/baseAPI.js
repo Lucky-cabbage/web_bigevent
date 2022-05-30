954e00f5b5aed0a3bec2cbf99023b317ac99132a
Lucky-cabbage <2199435336@qq.com>
2022-05-30 20:32:23 +0800

    完成主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,18 @@
 // 在这个函数中,可以拿到我们给ajax提供的配置对象
 $.ajaxPrefilter((options) => {
   options.url = 'http://www.liulongbin.top:3007' + options.url
+  if (options.url.includes('/my/')) {
+    options.headers = {
+      Authorization: localStorage.getItem('token')
+    };
+  }
+  // 每次发送请求回来校验token是否存在是否过期
+  options.complete = (res) => {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败!") {
+      //1.强制清空
+      localStorage.removeItem('token')
+      // 2.跳转到页面
+      location.href = '/login.html'
+    }
+  }
 })
